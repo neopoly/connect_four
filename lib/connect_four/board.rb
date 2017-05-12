5cd7f20be4f658c379318cfda72db5d0debe8d49
Eric Nordmeyer <en@v2a.net>
2017-05-12 10:40:36 +0200

    add functionality to put a stone in a column of the board

--- lib/connect_four/board.rb
+++ lib/connect_four/board.rb
@@ -9,5 +9,11 @@ module ConnectFour
       @fields = Array.new(ROWS) { Array.new(COLUMNS, '-')}
     end
 
+    def put_stone symbol, col
+      row = ROWS-1
+      row -= 1 until @fields[row].nil? || @fields[row][col-1] == "-"
+      @fields[row].nil? ? puts("This stack is packed") : @fields[row][col-1] = symbol
+    end
+
   end
 end
--- test/connect_four/board_test.rb
+++ test/connect_four/board_test.rb
@@ -12,5 +12,27 @@ class ConnectFour::BoardTest < ConnectFourSpec
       it("should only have '-'s as content") { board.fields.uniq.first.uniq.first.must_equal "-" }
     end
 
+    describe "#put_stone" do
+      before { board.put_stone('x', 3) }
+      it("should have that stone stored at the bottom of selected column") { board.fields[7][2].must_equal 'x' }
+
+      it "should have all other fields unaffected" do
+        board.fields.each_with_index do |row, y|
+          row.each_with_index do |field, x|
+            field.must_equal '-' unless y == 7 && x == 2
+          end
+        end
+      end
+
+      it("should store a second stone in the same column on top of the first") do
+        board.put_stone('x', 3)
+        board.fields[6][2].must_equal 'x'
+      end
+
+      it("shouldn't let you put more than 8 stones in a column") do
+        7.times { board.put_stone('x', 3) }
+        proc { board.put_stone('x', 3) }.must_output "This stack is packed\n"
+      end
+    end
   end
 end
