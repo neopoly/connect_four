4ffde292531742cb79b906f0fb39d7880d423f2a
Eric Nordmeyer <en@v2a.net>
2017-05-12 11:45:29 +0200

    change wording: use column for user input, col for Array index; use constant for blank field value

--- lib/connect_four/board.rb
+++ lib/connect_four/board.rb
@@ -2,17 +2,18 @@ module ConnectFour
   class Board
     attr_accessor :fields
 
+    BLANK = '-'
     COLUMNS = 8
     ROWS = 8
 
     def initialize
-      @fields = Array.new(ROWS) { Array.new(COLUMNS, '-')}
+      @fields = Array.new(ROWS) { Array.new(COLUMNS, BLANK)}
     end
 
-    def put_stone symbol, col
+    def put_stone symbol, column
       row = ROWS-1
-      row -= 1 until @fields[row].nil? || @fields[row][col-1] == "-"
-      @fields[row].nil? ? return : @fields[row][col-1] = symbol
+      row -= 1 until @fields[row].nil? || @fields[row][column-1] == BLANK
+      @fields[row].nil? ? return : @fields[row][column-1] = symbol
       return row
     end
 
