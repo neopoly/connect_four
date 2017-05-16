99723d9e4d984c12db5e40bb85af13662d5c2ad2
Eric Nordmeyer <en@v2a.net>
2017-05-16 11:05:42 +0200

    add test helper for easer fields setup in quartet verification tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,3 +10,7 @@ end
 def initial_board_string
   "1 2 3 4 5 6 7 8\n- - - - - - - -\n- - - - - - - -\n- - - - - - - -\n- - - - - - - -\n- - - - - - - -\n- - - - - - - -\n- - - - - - - -\n- - - - - - - -\n\n\n"
 end
+
+def setup_fields array_of_coordinates, symbol
+  array_of_coordinates.each { |coordinates| board.fields[coordinates.first][coordinates.last] = symbol }
+end
