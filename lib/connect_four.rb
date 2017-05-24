8e56896894a8f8e82bd3cf19ee94e26c788370c7
Mark Andrae <ma@protel.net>
2017-05-24 18:12:05 +0200

    added console app loop

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,22 +1,31 @@
-require "game/connect_four_game"
+require_relative "game/board"
+require_relative "game/connect_four_game"
 
 module ConnectFour
   def self.hello_world
     "hello world"
   end
 
-  def self.main
-    puts "Would you like to play a game?"
 
+  def self.main
+    exit_requested = false
+    Kernel.trap( "INT" ) do
+      Kernel.exit(0)
+    end
+
+    puts "Would you like to play a game?"
     current_game = ConnectFourGame.new
 
-    loop do
-      puts
-      input = gets.chomp
-      command = input.split(/\s/)
+    while !exit_requested do
+
+
+      puts current_game.board.to_s
+      input = gets.chomp.to_s
+      command = input.strip
+
 
       case command
-        when /\Ahelp\z/i
+        when 'help'
           puts "super game help text for winners"
         else puts 'Invalid command'
       end
