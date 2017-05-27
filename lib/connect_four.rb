4f28429fff2754b9cbf3544c27547da5becf3f42
Die-Will <diego.will@hotmail.de>
2017-05-28 00:59:49 +0200

    added function to reset the game's board

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -2,4 +2,7 @@ module ConnectFour
   def self.hello_world
     "hello world"
   end
+  def self.reset_board(board)
+    board = Array.new(8) { Array.new(8, ".") }
+  end
 end
--- test/connect_four_test.rb
+++ test/connect_four_test.rb
@@ -4,4 +4,8 @@ class ConnectFourTest < ConnectFourSpec
   def test_hello_world
     assert_equal "hello world", ConnectFour.hello_world
   end
+  def test_reset_board
+    board = Array.new(8) { Array.new(8, ".") }
+    assert_equal board, ConnectFour.reset_board(board)
+  end
 end
