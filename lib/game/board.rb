0810753130239d9f9baf272ead6c67421d01ea08
Mark Andrae <ma@protel.net>
2017-05-27 19:37:35 +0200

    Board + ConncectFourGame added basic test and insert_at function to drop game pieces

--- lib/game/board.rb
+++ lib/game/board.rb
@@ -11,6 +11,28 @@ class Board
     @win_condition_length = winning_length
   end
 
+  def insert_char_at(character,column_number)
+    x = column_number - 1
+    y = find_drop_point x
+    if y < 0
+      raise "Column full"
+    end
+    @fields.send(:[]=, y, x, character)
+
+  end
+
+  def find_drop_point(x)
+    column = @fields.column x
+    max_index = column.size - 1
+    0.upto max_index do |y|
+      if column[y] != EMPTY_FIELD
+        return (y - 1)
+      end
+    end
+
+    max_index
+  end
+
   def win_condition_met?
     (fields.row_count - 1).downto 0 do |x|
       0.upto(fields.column_count - 1) do |y|
--- test/game/game_test.rb
+++ test/game/game_test.rb
@@ -2,7 +2,17 @@ require 'test_helper'
 require 'game/connect_four_game'
 
 class GameTest < ConnectFourTestHelper
+
+  def setup
+    @game = ConnectFourGame.new
+  end
+
   def test_new_game
-    assert_equal "new", ConnectFourGame.new.state
+    assert_equal "new", @game.state
+
+    @game.start
+    @game.make_move 4
+
+    assert_equal "player_2_move", @game.state
   end
 end
