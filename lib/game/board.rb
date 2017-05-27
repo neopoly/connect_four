2bbacd756d28f30baae82d885de94c86a5049f52
Mark Andrae <ma@protel.net>
2017-05-27 20:25:54 +0200

    updated Board: improved code style

--- lib/game/board.rb
+++ lib/game/board.rb
@@ -6,20 +6,16 @@ class Board
   attr_accessor :fields
 
   def initialize(num_rows, num_cols, winning_length)
-    #initialize board as '.'  filled matrix
+    # initialize board as '.'  filled matrix
     @fields = Matrix.build(num_rows, num_cols) { EMPTY_FIELD }
     @win_condition_length = winning_length
   end
 
   def insert_char_at(character, column_number)
-    if column_number < 1 || column_number > @fields.column_count
-      raise 'Invalid column number'
-    end
+    raise 'Invalid column number' if column_number < 1 || column_number > @fields.column_count
     x = column_number - 1
     y = find_drop_point x
-    if y < 0
-      raise 'Column full'
-    end
+    raise 'Column full' if y < 0
     @fields.send(:[]=, y, x, character)
   end
 
@@ -27,9 +23,7 @@ class Board
     column = @fields.column x
     max_index = column.size - 1
     0.upto max_index do |y|
-      if column[y] != EMPTY_FIELD
-        return (y - 1)
-      end
+      return (y - 1) if column[y] != EMPTY_FIELD
     end
 
     max_index
@@ -38,9 +32,7 @@ class Board
   def win_condition_met?
     (fields.row_count - 1).downto 0 do |x|
       0.upto(fields.column_count - 1) do |y|
-        if winning_line_at? x, y
-          return true
-        end
+        return true if winning_line_at? x, y
       end
     end
 
@@ -56,17 +48,13 @@ class Board
 
   def is_begin_of_line?(row, col, row_step, col_step)
     start_val = @fields[row, col]
-    if start_val == EMPTY_FIELD
-      return false
-    end
+    return false if start_val == EMPTY_FIELD
 
     1.upto(@win_condition_length - 1) do |i|
       test_row = row + i * row_step
       test_col = col + i * col_step
-      #puts "from: #{col},#{row}, check: #{test_col},#{test_row}"
-      if @fields[test_row, test_col] != start_val
-        return false
-      end
+      # puts "from: #{col},#{row}, check: #{test_col},#{test_row}"
+      return false if @fields[test_row, test_col] != start_val
     end
     true
   end
