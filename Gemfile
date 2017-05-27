cb607d5365a226e2207f7151eb0e4d0aa31449cf
Mark Andrae <ma@protel.net>
2017-05-27 19:34:46 +0200

    class ConnectFourGame added statemachine state machine for gamestate handling and transitions between states

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source :rubygems
 
 gem 'rake'
 gem 'minitest', '~> 5.10'
+gem 'state_machines'
--- lib/game/connect_four_game.rb
+++ lib/game/connect_four_game.rb
@@ -1,12 +1,41 @@
+require 'state_machines'
+
 class ConnectFourGame
 
-  attr_accessor :state, :board
+  SYMBOL_PLAYER_1 = 'x'
+  SYMBOL_PLAYER_2 = 'o'
 
-  def initialize()
-    @board = Board.new(8,8,4)
-    @state = "new"
+  attr_accessor :board
+  state_machine :state, initial: :new do
+
+    event :start do
+      transition :new => :player_1_move
+    end
+
+    event :make_move do
+      transition new: :player_1_move, player_1_move: :player_2_move,
+                 player_2_move: :player_1_move
+    end
+
+    event :win do
+      transition all => :finished
+    end
+
+    event :no_more_moves do
+      transition all => :finished
+    end
+
   end
 
+  def make_move(column_number, *args)
+    super(*args)
+    symbol = self.player_1_move? ? SYMBOL_PLAYER_1 : SYMBOL_PLAYER_2
+    @board.insert_char_at symbol, column_number
+  end
 
+  def initialize()
+    @board = Board.new 8, 8, 4
+    super()
+  end
 
 end
