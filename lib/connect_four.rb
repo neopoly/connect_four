5301b5342c15161d8fe091f2f6a9bbf5ba84cf91
Die-Will <diego.will@hotmail.de>
2017-05-29 14:30:11 +0200

    added Board class and moved relevant functions to the new class. Also edited the tests to make use of this new class

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -2,15 +2,13 @@ module ConnectFour
   def self.hello_world
     "hello world"
   end
-  def self.reset_board(board)
-    board = Array.new(8) { Array.new(8, ".") }
-  end
-  def self.is_valid_move(board, column)
-    board[column-1].include?(".")
-  end
+  #def self.reset_board(board)
+  #  board.resetBoard()
+  #end
+  #def self.is_valid_move(board, column)
+  #  board.isValidMove(column)
+  #end
   def self.make_move(board, column, token)
-    free_index = board[column-1].rindex(".")
-    board[column-1][free_index] = token
-    return board[column-1][free_index]
+    board.makeMove(column, token)
   end
 end
--- test/connect_four_test.rb
+++ test/connect_four_test.rb
@@ -1,22 +1,38 @@
 require 'test_helper'
+require 'board'
 
 class ConnectFourTest < ConnectFourSpec
   def test_hello_world
     assert_equal "hello world", ConnectFour.hello_world
   end
-  def test_reset_board
-    board = Array.new(8) { Array.new(8, ".") }
-    full_board = Array.new(8) { Array.new(8, "x") }
-    assert_equal board, ConnectFour.reset_board(full_board)
+  def test_make_move
+    board = Board.new()
+    assert_equal true, board.makeMove(1, "x")
   end
   def test_is_valid_move
-    board = Array.new(8) { Array.new(8, "x") }
-    board[0][0] = "."
-    assert_equal true, ConnectFour.is_valid_move(board, 1)
-    assert_equal false, ConnectFour.is_valid_move(board, 2)
-  end
-  def test_make_move
-    board = Array.new(8) { Array.new(8, ".") }
-    assert_equal "x", ConnectFour.make_move(board, 1, "x")
+    board = Board.new()
+    board.makeMove(1, "x")
+    board.makeMove(1, "o")
+    board.makeMove(1, "x")
+    board.makeMove(1, "o")
+    board.makeMove(1, "x")
+    board.makeMove(1, "o")
+    board.makeMove(1, "x")
+    board.makeMove(1, "o")
+    assert_equal false, board.isValidMove(1)
+    assert_equal true, board.isValidMove(2)
+  end 
+  def test_reset_board
+    board = Board.new()
+    board.makeMove(1, "x")
+    board.makeMove(1, "o")
+    board.makeMove(1, "x")
+    board.makeMove(1, "o")
+    board.makeMove(1, "x")
+    board.makeMove(1, "o")
+    board.makeMove(1, "x")
+    board.makeMove(1, "o")
+    board.resetBoard()
+    assert_equal true, board.isValidMove(1)
   end
 end
