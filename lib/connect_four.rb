3240975dd8828b67fc5746b9189866cde0f67295
weifanhaha <aily34809@gmail.com>
2018-05-05 10:48:41 +0200

    add function of init board and put mark

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -2,4 +2,24 @@ module ConnectFour
   def self.hello_world
     "hello world"
   end
+
+  def self.init_board(dim=8)
+    @dim = dim
+    @board = []
+    @dim.times do
+      @board.push(Array.new(8,'.'))
+    end
+    @board
+  end
+
+  def self.put_mark(mark, col)
+    for row in 0..@dim-1
+      if @board[row][col] != '.'
+        @board[row-1][col] = mark
+        return @board
+      end
+    end
+    @board[@dim-1][col] = mark
+    @board
+  end
 end
