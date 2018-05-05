3e8a303b8f9a7311baae72ec7027fdebc3d38a16
weifanhaha <aily34809@gmail.com>
2018-05-05 11:01:44 +0200

    add function for switching players

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -3,8 +3,9 @@ module ConnectFour
     "hello world"
   end
 
-  def self.init_board(dim=8)
+  def self.init_board(dim=8, player=1)
     @dim = dim
+    @player = player
     @board = []
     @dim.times do
       @board.push(Array.new(8,'.'))
@@ -16,10 +17,27 @@ module ConnectFour
     for row in 0..@dim-1
       if @board[row][col] != '.'
         @board[row-1][col] = mark
+        self.switch_turn
         return @board
       end
     end
     @board[@dim-1][col] = mark
+    self.switch_turn
     @board
+  end
+
+  def self.switch_turn
+    if @player == 1
+      @player = 2
+    elsif @player == 2
+      @player = 1
+    else
+      return false
+    end
+    true
+  end
+
+  def self.check_turn
+    @player
   end
 end
