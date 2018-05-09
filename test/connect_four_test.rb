81fb34f81accaa53232d3d83ed4011a706194628
weifanhaha <aily34809@gmail.com>
2018-05-09 09:11:00 +0200

    Use a more object-oriented way to build the game and  modify the test file

--- test/connect_four_test.rb
+++ test/connect_four_test.rb
@@ -1,11 +1,10 @@
 require 'test_helper'
 
 class ConnectFourTest < ConnectFourSpec
-  def test_hello_world
-    assert_equal "hello world", ConnectFour.hello_world
-  end
 
-  # Test for initialize and put the mark
+  #####################################
+  # Test of Gameboard
+  #####################################
   def test_init_board
     board = [[".", ".", ".", ".", ".", ".", ".", "."],
              [".", ".", ".", ".", ".", ".", ".", "."],
@@ -16,7 +15,7 @@ class ConnectFourTest < ConnectFourSpec
              [".", ".", ".", ".", ".", ".", ".", "."],
              [".", ".", ".", ".", ".", ".", ".", "."]]
 
-    assert_equal board, ConnectFour.init_board
+    assert_equal board, ConnectFour::GameBoard.new.board
 
     board = [[".", ".", ".", ".", "."],
              [".", ".", ".", ".", "."],
@@ -24,10 +23,10 @@ class ConnectFourTest < ConnectFourSpec
              [".", ".", ".", ".", "."],
              [".", ".", ".", ".", "."]]
 
-    assert_equal board, ConnectFour.init_board(dim=5)
+    assert_equal board, ConnectFour::GameBoard.new(dim=5).board
   end
-
   def test_put_mark
+    game_board = ConnectFour::GameBoard.new
     board = [[".", ".", ".", ".", ".", ".", ".", "."],
              [".", ".", ".", ".", ".", ".", ".", "."],
              [".", ".", ".", ".", ".", ".", ".", "."],
@@ -37,9 +36,9 @@ class ConnectFourTest < ConnectFourSpec
              [".", ".", ".", ".", ".", ".", ".", "."],
              ["x", ".", ".", ".", ".", ".", ".", "."]]
     col = 0
-    ConnectFour.init_board
-    assert ConnectFour.put_mark('x',col)
-    assert_equal board, ConnectFour.current_board
+
+    assert game_board.put_mark('x',col)
+    assert_equal board, game_board.board
 
     board = [[".", ".", ".", ".", ".", ".", ".", "."],
              [".", ".", ".", ".", ".", ".", ".", "."],
@@ -49,110 +48,160 @@ class ConnectFourTest < ConnectFourSpec
              [".", ".", ".", ".", ".", ".", ".", "."],
              ["o", ".", ".", ".", ".", ".", ".", "."],
              ["x", ".", ".", ".", ".", ".", ".", "."]]
-    assert ConnectFour.put_mark('o',col)
-    assert_equal board, ConnectFour.current_board
-  end
-
-  # Test for switching players and check the turn
-  def test_check_turn
-    ConnectFour.init_board
-    assert_equal 1, ConnectFour.check_turn
-    ConnectFour.put_mark('x', 0)
-    ConnectFour.switch_turn
-    assert_equal 2, ConnectFour.check_turn
-
-    ConnectFour.init_board(dim=8, player=2)
-    assert_equal 2, ConnectFour.check_turn
-
+    assert game_board.put_mark('o',col)
+    assert_equal board, game_board.board
   end
 
   # Test for game over and connection
   def test_connect_row
-    ConnectFour.init_board
-    ConnectFour.put_mark('x', 0)
-    ConnectFour.put_mark('x', 1)
-    ConnectFour.put_mark('x', 2)
-    ConnectFour.put_mark('x', 3)
-    assert (not ConnectFour.full_board?)
-    assert ConnectFour.connect_row?
-    assert ConnectFour.game_over?
+    game_board = ConnectFour::GameBoard.new
+    game_board.put_mark('x', 0)
+    game_board.put_mark('x', 1)
+    game_board.put_mark('x', 2)
+    game_board.put_mark('x', 3)
+    assert (not game_board.full_board?)
+    assert game_board.connect_row?
   end
 
   def test_connect_col
-    ConnectFour.init_board
-    ConnectFour.put_mark('x', 0)
-    ConnectFour.put_mark('x', 0)
-    ConnectFour.put_mark('x', 0)
-    ConnectFour.put_mark('x', 0)
-    assert ConnectFour.connect_col?
-    assert ConnectFour.game_over?
+    game_board = ConnectFour::GameBoard.new
+    game_board.put_mark('x', 0)
+    game_board.put_mark('x', 0)
+    game_board.put_mark('x', 0)
+    game_board.put_mark('x', 0)
+    assert game_board.connect_col?
   end
 
   def test_connect_diag
-    ConnectFour.init_board
-    ConnectFour.put_mark('x', 0)
-    ConnectFour.put_mark('o', 1)
-    ConnectFour.put_mark('x', 1)
-    ConnectFour.put_mark('o', 2)
-    ConnectFour.put_mark('o', 2)
-    ConnectFour.put_mark('x', 2)
-    ConnectFour.put_mark('o', 3)
-    ConnectFour.put_mark('o', 3)
-    ConnectFour.put_mark('o', 3)
-    ConnectFour.put_mark('x', 3)
-    assert ConnectFour.connect_diag?
-    assert ConnectFour.game_over?
+    game_board = ConnectFour::GameBoard.new
+    game_board.put_mark('x', 0)
+    game_board.put_mark('o', 1)
+    game_board.put_mark('x', 1)
+    game_board.put_mark('o', 2)
+    game_board.put_mark('o', 2)
+    game_board.put_mark('x', 2)
+    game_board.put_mark('o', 3)
+    game_board.put_mark('o', 3)
+    game_board.put_mark('o', 3)
+    game_board.put_mark('x', 3)
+    assert game_board.connect_diag?
   end
 
   def test_full_board
-    ConnectFour.init_board
+    game_board = ConnectFour::GameBoard.new
     # fill the board
-    for col in (0..7).step(4)
-      for row in 0..7
-        if ConnectFour.check_turn == 1
-          ConnectFour.put_mark('x', col)
-          ConnectFour.put_mark('x', col+1)
-          ConnectFour.put_mark('o', col+2)
-          ConnectFour.put_mark('o', col+3)
-          ConnectFour.switch_turn
+    for row in 0..7
+      for col in (0..7).step(4)
+        if row % 2 == 0
+          game_board.put_mark('x', col)
+          game_board.put_mark('x', col+1)
+          game_board.put_mark('o', col+2)
+          game_board.put_mark('o', col+3)
         else
-          ConnectFour.put_mark('o', col)
-          ConnectFour.put_mark('o', col+1)
-          ConnectFour.put_mark('x', col+2)
-          ConnectFour.put_mark('x', col+3)
-          ConnectFour.switch_turn
+          game_board.put_mark('o', col)
+          game_board.put_mark('o', col+1)
+          game_board.put_mark('x', col+2)
+          game_board.put_mark('x', col+3)
         end
       end
     end
 
-    assert (not ConnectFour.connect_row?)
-    assert (not ConnectFour.connect_col?)
-    assert (not ConnectFour.connect_diag?)
-    assert ConnectFour.full_board?
-    assert ConnectFour.game_over?
+    assert (not game_board.connect_row?)
+    assert (not game_board.connect_col?)
+    assert (not game_board.connect_diag?)
+    assert game_board.full_board?
   end
 
-  # check winner
-  def test_check_winner
-    ConnectFour.init_board
-    6.times do
-      # player 1
-      if ConnectFour.check_turn == 1
-        if ConnectFour.put_mark('x', 0)
-          ConnectFour.switch_turn
-        end
-      # player 2
-      elsif ConnectFour.check_turn == 2
-        if ConnectFour.put_mark('o', 1)
-          ConnectFour.switch_turn
+  #####################################
+  # Test of Game
+  #####################################
+  def test_init_board_by_new_game
+    board = ConnectFour::GameBoard.new.board
+    assert_equal board, ConnectFour::Game.new.game_board.board
+  end
+
+  def test_check_turn
+    game = ConnectFour::Game.new
+    assert_equal 1, game.player
+    game.switch_turn
+    assert_equal 2, game.player
+
+    game = ConnectFour::Game.new(dim=8, player=2)
+    assert_equal 2, game.player
+  end
+
+  def test_game_over_by_connecting_row
+    game = ConnectFour::Game.new
+    game.game_board.put_mark('x', 0)
+    game.game_board.put_mark('x', 1)
+    game.game_board.put_mark('x', 2)
+    game.game_board.put_mark('x', 3)
+    assert game.game_board.connect_row?
+    assert game.game_over?
+  end
+
+  def test_game_over_by_connecting_col
+    game = ConnectFour::Game.new
+    game.game_board.put_mark('x', 0)
+    game.game_board.put_mark('x', 0)
+    game.game_board.put_mark('x', 0)
+    game.game_board.put_mark('x', 0)
+    assert game.game_board.connect_col?
+    assert game.game_over?
+  end
+
+  def test_game_over_by_connecting_diag
+    game = ConnectFour::Game.new
+    game.game_board.put_mark('x', 0)
+    game.game_board.put_mark('o', 1)
+    game.game_board.put_mark('x', 1)
+    game.game_board.put_mark('o', 2)
+    game.game_board.put_mark('o', 2)
+    game.game_board.put_mark('x', 2)
+    game.game_board.put_mark('o', 3)
+    game.game_board.put_mark('o', 3)
+    game.game_board.put_mark('o', 3)
+    game.game_board.put_mark('x', 3)
+    assert game.game_board.connect_diag?
+    assert game.game_over?
+  end
+
+  def test_game_over_by_full_board
+    game = ConnectFour::Game.new
+    for row in 0..7
+      for col in (0..7).step(4)
+        if row % 2 == 0
+          game.game_board.put_mark('x', col)
+          game.game_board.put_mark('x', col+1)
+          game.game_board.put_mark('o', col+2)
+          game.game_board.put_mark('o', col+3)
+        else
+          game.game_board.put_mark('o', col)
+          game.game_board.put_mark('o', col+1)
+          game.game_board.put_mark('x', col+2)
+          game.game_board.put_mark('x', col+3)
         end
       end
     end
+    assert game.game_board.full_board?
+    assert game.game_over?
+  end
 
-    if ConnectFour.put_mark('x', 0)
-      assert ConnectFour.game_over?
-      assert ConnectFour.check_winner == 1
+  def test_check_winner
+    game = ConnectFour::Game.new
+    6.times do
+      # player 1
+      if game.player == 1
+        game.game_board.put_mark('x', 0)
+        game.switch_turn
+      # player 2
+      elsif game.player == 2
+        game.game_board.put_mark('o', 1)
+        game.switch_turn
+      end
     end
-
+    game.game_board.put_mark('x', 0)
+    assert game.game_over?
+    assert game.check_winner == 1
   end
 end
