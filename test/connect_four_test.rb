7ac996945c048036658cd06ea196ce25da14dab5
Kevin Kahlke <kk@neopoly.de>
2020-01-17 14:37:47 +0100

    Test class fixed.

--- test/connect_four_test.rb
+++ test/connect_four_test.rb
@@ -2,32 +2,14 @@ require 'test_helper'
 require "minitest/autorun"
 
 class ConnectFourTest < ConnectFourSpec
-
-  describe CLI do
-    it "reads input" do
-      input = StringIO.new("test 1 2 3")
-      cli = CLI.new(input, nil)
-
-      value = cli.readInput
-
-      expect("test 1 2 3").must_equal value
-    end
-    it "prints to stdout" do
-      output = StringIO.new
-      cli = CLI.new(nil, output)
-
-      cli.printOut("test 1 2 3")
-
-      expect("test 1 2 3").must_equal output.string
-    end
-  end
   
   def setup
+    @cli = ConnectFour::CLI.new
     @board = ConnectFour::Board.new
     @test = (1..8).inject([]) {|a,i| a << []}
     @player1 = ConnectFour::Player.new("Kevin", "x")
     @player2 = ConnectFour::Player.new("Schmevin", "o")
-    @game = ConnectFour::Game.new @board, @player1, @player2
+    @game = ConnectFour::Game.new @cli, @board, @player1, @player2
   end
 
   def test_version
